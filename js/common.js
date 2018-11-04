48d48f5af657dc6d7ec325812d56596406d79b71
Pavel Hvatsik <pavel.hvatsik@yandex.ru>
2018-11-04 21:34:31 +0300

    04/11 adaptived for width 1024px

--- js/common.js
+++ js/common.js
@@ -1,26 +1,79 @@
-let products = document.querySelector('.products');
-let productsItem = document.getElementsByClassName('products__item');
-let widthItem = productsItem[0].offsetWidth;
-let arrowLeft = document.querySelector('.arrow_left');
-let arrowRight = document.querySelector('.arrow_right');
-let marginItem = 43.333;
-let slideWidth = +widthItem + marginItem;
-let i = 0;
+$(window).on('load', function() {
+	$('#preloader').fadeOut('slow');
+});
+//searching
+$('.search__close').on('click', function() {
+	$('.search').removeClass('search-active');
+});
+$('.button-second').on('click', function() {
+	$('.search').addClass('search-active');
+});
 
-console.log(productsItem.length);
+//burger menu
+$('.drop__menu').click(function() {
+	$('.header__nav').toggleClass('mobile_active');
+});
+$(document).on('click', function(event) {
+	if ($(event.target).closest(".drop__menu, .header__nav").length) return;
+	$('.header__nav').removeClass('mobile_active');
+});
 
-arrowRight.onclick = function() {
-	i -= slideWidth;
-	if (i < -slideWidth * 7) {
-		i = 0;
-	}
-	products.style.marginLeft = i + "px";
+//droplink
+var dropButton = $('.drop_button');
+dropButton.click(function() {
+	var dataDrop = $(this).attr('data-drop'); //при клике достаем значение data-tab
+	// $('.mobile__dropLinks').css('display', 'none');
+	$('.' + dataDrop).slideToggle(0);
+});
+
+//footer drop links
+var dropButtonFooter = $('.open_links');
+dropButtonFooter.click(function() {
+	var dataDrop = $(this).attr('data-drop'); //при клике достаем значение data-tab
+	// $('.mobile__dropLinks').css('display', 'none');
+	$('.' + dataDrop).toggleClass('footer_active');
+});
+
+$('.slider').slick({
+	dots: true,
+	arrows: false,
+	infinite: false
+});
+
+function products() {
+	$('.products').slick({
+		slidesToShow: 4,
+		infinite: false,
+		variableWidth: true,
+		responsive: [{
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 3
+			}
+		}]
+	});
+};
+
+function slickify() {
+	$('.brands').slick({
+		responsive: [{
+			breakpoint: 5000,
+			settings: "unslick"
+		}, {
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 5,
+				variableWidth: true
+			}
+		}]
+	});
 }
-
-arrowLeft.onclick = function() {
-	i += slideWidth;
-	if (i > 0) {
-		i = 0;
+slickify();
+products();
+$(window).resize(function() {
+	var $windowWidth = $(window).width();
+	if ($windowWidth < 1024) {
+		slickify();
+		products();
 	}
-	products.style.marginLeft = i + "px";
-}
+});
