60ed99fa1194d3184484db52804cc9c7eb0ed5ba
Pavel Hvatsik <pavel.hvatsik@yandex.ru>
2018-11-17 16:15:46 +0300

    coments added js files

--- js/common.js
+++ js/common.js
@@ -1,1 +1,103 @@
-$(window).on("load",function(){$("#preloader").fadeOut("slow")}),$(".search__close").on("click",function(){$(".search").removeClass("search-active")}),$(".button-second").on("click",function(){$(".search").addClass("search-active")}),$(".drop__menu").click(function(){$(".header__nav").toggleClass("mobile_active")}),$(document).on("click",function(o){$(o.target).closest(".drop__menu, .header__nav").length||$(".header__nav").removeClass("mobile_active")});var dropButton=$(".drop_button");dropButton.click(function(){var o=$(this).attr("data-drop");$("."+o).slideToggle(0)});var dropButtonFooter=$(".open_links");function products(){$(".products").slick({slidesToShow:4,infinite:!1,variableWidth:!0,responsive:[{breakpoint:1023,settings:{slidesToShow:3}},{breakpoint:767,settings:{slidesToShow:2}},{breakpoint:479,settings:{slidesToShow:1}}]})}function brands(){$(".brands").slick({responsive:[{breakpoint:5e3,settings:"unslick"},{breakpoint:1023,settings:{slidesToShow:5,variableWidth:!0}},{breakpoint:767,settings:{slidesToShow:4,variableWidth:!0}},{breakpoint:479,settings:{slidesToShow:3,variableWidth:!0}}]})}dropButtonFooter.click(function(){var o=$(this).attr("data-drop");$("."+o).toggleClass("footer_active")}),$(".slider").slick({dots:!0,arrows:!1,infinite:!1}),brands(),products(),$(window).resize(function(){$(window).width()<1024&&(slickify(),products())});
+//прелоадер
+$(window).on('load', function() {
+	$('#preloader').fadeOut('slow'); //когда загружается страница, то прелоадер исчезает
+});
+
+//поиск
+$('.search__close').on('click', function() {
+	$('.search').removeClass('search-active'); //при нажатии на крестик закрывает инпут поиска
+});
+$('.button-second').on('click', function() {
+	$('.search').addClass('search-active'); //при нажатии на поиск открывается инпут поиска
+});
+
+//бургер меню
+$('.drop__menu').click(function() {
+	$('.header__nav').toggleClass('mobile_active'); //открывает и закрывает меню
+});
+$(document).on('click', function(event) {
+	if ($(event.target).closest(".drop__menu, .header__nav").length) return;
+	$('.header__nav').removeClass('mobile_active'); //если кликнули вне блока меню,то закрывает его
+});
+
+//выпадающие ссылки
+$('.drop_button').click(function() {
+	var dataDrop = $(this).attr('data-drop'); //при клике достаем значение data-tab
+	$('.' + dataDrop).slideToggle(0); //октрывает или закрывает категорию меню
+});
+
+//выпадающие ссылки футер
+$('.open_links').click(function() {
+	var dataDrop = $(this).attr('data-drop'); //при клике достаем значение data-tab
+	$('.' + dataDrop).toggleClass('footer_active'); //октрывает или закрывает категорию меню
+});
+
+//главный слайдер
+$('.slider').slick({
+	dots: true,
+	arrows: false,
+	infinite: false
+});
+
+//слайдер товаров
+function products() {
+	$('.products').slick({
+		slidesToShow: 4,
+		infinite: false,
+		variableWidth: true,
+		responsive: [{
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 3
+			}
+		}, {
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2
+			}
+		}, {
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 1
+			}
+		}]
+	});
+};
+
+//слайдер брендов
+function brands() {
+	$('.brands').slick({
+		responsive: [{
+			breakpoint: 5000,
+			settings: "unslick"
+		}, {
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 5,
+				variableWidth: true
+			}
+		}, {
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 4,
+				variableWidth: true
+			}
+		}, {
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 3,
+				variableWidth: true
+			}
+		}]
+	});
+}
+brands(); //вызов слайдера брендов
+products(); //вызов слайдера товаров
+
+$(window).resize(function() {
+	var $windowWidth = $(window).width();
+	if ($windowWidth < 1024) { //при ширине окна меньше 1024px обновлять функции слайдеров бренда и товаров
+		slickify();
+		products();
+	}
+});
--- js/text.js
+++ js/text.js
@@ -1,1 +1,22 @@
-var content={size:17,newsContent:document.getElementsByClassName("products__link"),buildText:function(){for(var t=0;t<this.newsContent.length;t++){var e=this.newsContent[t].textContent;e.length>this.size&&(this.newsContent[t].innerHTML=e.slice(0,this.size)+"...")}}},blogNews={size:50,newsContent:document.getElementsByClassName("item__title"),buildText:function(){for(var t=0;t<this.newsContent.length;t++){var e=this.newsContent[t].textContent;e.length>this.size&&(this.newsContent[t].innerHTML=e.slice(0,this.size)+"...")}}};content.buildText(),blogNews.buildText();
+var content = {
+	size: 17, //количество символов
+	newsContent: document.getElementsByClassName("products__link"), //блок,где урезается текст
+	buildText: function() {
+		for (var t = 0; t < this.newsContent.length; t++) {
+			var e = this.newsContent[t].textContent;
+			e.length > this.size && (this.newsContent[t].innerHTML = e.slice(0, this.size) + "...") //урезаем текст до size и в конце ставим троеточие
+		}
+	}
+};
+var blogNews = {
+	size: 50,
+	newsContent: document.getElementsByClassName("item__title"),
+	buildText: function() {
+		for (var t = 0; t < this.newsContent.length; t++) {
+			var e = this.newsContent[t].textContent;
+			e.length > this.size && (this.newsContent[t].innerHTML = e.slice(0, this.size) + "...")
+		}
+	}
+};
+content.buildText();
+blogNews.buildText();
